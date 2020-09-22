e2d3579798283916b7a16f42fd0a7a69338c6229
rahulap369 <rahulap369@gmail.com>
2020-09-22 16:02:41 +1000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Live young... Live today....</p>
+        <p>Live today....</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
